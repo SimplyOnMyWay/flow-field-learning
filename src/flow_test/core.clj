167c95b889bd638d50211a72ac5c3cc50504356e
SimplyOnMyWay <micheal.ocathain@gmail.com>
2021-04-27 18:22:31 +0100

    Draw single vector near any grid point with angle

--- src/flow_test/core.clj
+++ src/flow_test/core.clj
@@ -2,7 +2,17 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(def res-factor 0.01)
+(def res-factor 0.025)
+(def num-steps-global 50)
+(def step-length-global 20)
+
+(defn screen-offsets
+  "set left-x, right-x, top-y and bottom-y"
+  []
+  {:left-x (* -0.5 (q/width))
+   :right-x (* 1.5 (q/width))
+   :top-y (* -0.5 (q/height))
+   :bottom-y (* 1.5 (q/height))})
 
 (defn unify-x-y-angle
   [x y angle]
@@ -10,11 +20,11 @@
 
 (defn setup-grid
   []
-  (let [resolution (* (q/width) 0.025)
-        left-x (* -0.5 (q/width))
-        right-x (* 1.5 (q/width))
-        top-y (* -0.5 (q/height))
-        bottom-y (* 1.5 (q/height))
+  (let [resolution (* (q/width) res-factor)
+        left-x (:left-x (screen-offsets))
+        right-x (:right-x (screen-offsets))
+        top-y (:top-y (screen-offsets))
+        bottom-y (:bottom-y (screen-offsets))
         num-columns (/ (- right-x left-x) resolution)
         num-rows (/ (- bottom-y top-y) resolution)
         default-angle q/PI
@@ -44,30 +54,90 @@
             (q/ellipse x y 3 3)
             (q/line x y (+ x (* line-length (q/cos angle))) (+ y (* line-length (q/sin angle))))))))))
 
-(defn setup []
-  (q/frame-rate 30)
-  (q/color-mode :hsb 360 100 100 1.0)
-  (setup-grid))
+
+(defn get-angle
+  [x y state]
+  (:angle (first (filter #(and (= (:x %) x) (= (:y %) y)) state))))
 
 (defn update-grid-point
   "leave :x and :y as they are, and adjust :angle"
   [grid-point]
-  (let [angle-inc 0.014] 
+  (let [resolution (* (q/width) res-factor)
+        left-x (:left-x (screen-offsets))
+        right-x (:right-x (screen-offsets))
+        top-y (:top-y (screen-offsets))
+        bottom-y (:bottom-y (screen-offsets))
+        num-columns (/ (- right-x left-x) resolution)
+        num-rows (/ (- bottom-y top-y) resolution)] 
     {:x (:x grid-point)
      :y (:y grid-point)
-     :angle (+ angle-inc (:angle grid-point))}))
+     :angle (* (/ (/ (:y grid-point) (* (q/width) res-factor)) num-rows) q/PI)}))
 
 (defn update-state [state]
   (map update-grid-point state))
 
+(defn setup []
+  (q/frame-rate 30)
+  (q/color-mode :hsb 360 100 100 1.0)
+  (map update-grid-point (setup-grid)))
+
+(defn x-offset
+  [x]
+  (- x (:left-x (screen-offsets))))
+
+(defn y-offset
+  [y]
+  (- y (:left-y (screen-offsets))))
+
+(defn draw-curve-recursive
+  "use recur to implement curve"
+  [grid x-start y-start num-steps step-length]
+  (let [resolution (* (q/width) res-factor)
+        p1 {:x x-start :y y-start}
+        n num-steps
+        ;grid-angle q/QUARTER-PI
+        grid-angle (:angle (first (filter #(and (<= (q/abs (- (:x %) (:x p1))) resolution) (<= (q/abs (- (:y %) (:y p1))) resolution)) grid)))
+        x-step (* step-length (q/cos grid-angle))
+        y-step (* step-length (q/sin grid-angle))
+        p2 {:x (+ (:x p1) x-step) :y (+ (:y p1) y-step)}]
+    (q/with-stroke [0 100 100 1.0]
+      (q/stroke-weight 3)
+      (q/line (:x p1) (:y p1) (:x p2) (:y p2)))))
+
+
+(comment
+  (def g (setup-grid))
+  (map update-grid-point g)
+  (defn check-angle
+    []
+    (let [resolution (* 900 res-factor)
+          p1 {:x 200 :y 400}]
+      (:angle (first (filter #(and (<= (q/abs (- (:x %) (:x p1))) resolution) (<= (q/abs (- (:y %) (:y p1))) resolution)) g))))))
+
+
+
+(comment
+  (defn draw-curve
+    "draw curve through point at angle a for a distance d"
+    [x-start y-start]
+    (q/begin-shape)
+    (doseq [n (range num-steps)]
+      (let [x-offset ()])
+      (q/curve-vertex x-start y-start)
+      
+      (q/curve-vertex x-start y-start)
+      (q/curve-vertex 100 80)
+      (q/curve-vertex 100 80))
+    (q/end-shape)))
+
+
 (defn draw-state [state]
-  (q/no-loop)
+  ;(q/no-loop)
   (q/background 50 80 80 0.7)
   (q/with-fill nil
     (q/with-stroke [200 0 100 1.0]
       (draw-grid-angle state)
-      ;(map update-grid-point state)
-      )))
+      (draw-curve-recursive state 800 600 num-steps-global step-length-global))))
 
 (q/defsketch flow-test
   :title "flow-test!"
